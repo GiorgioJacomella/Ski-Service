d489d40b6c2697ff2094f9355bde11465102acb6
Giorgio <91681501+GiorgioJacomella@users.noreply.github.com>
2023-11-14 16:49:56 +0100

    1.05 Login API with JWT

--- SkiService-Backend/SkiService-Backend/Controllers/loginController.cs
+++ SkiService-Backend/SkiService-Backend/Controllers/loginController.cs
@@ -1,6 +1,62 @@
-﻿namespace SkiService_Backend.Controllers
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SkiService_Backend.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+namespace SkiService_Backend.Controllers
 {
-    public class loginController
+    [Route("api/login")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
     {
+        private readonly registrationContext _context;
+        private readonly IConfiguration _configuration;
+
+        // Inject the configuration into your controller
+        public AuthenticationController(registrationContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> PostLogin(LoginModel loginModel)
+        {
+            var userInfo = await _context.UserInfos.FindAsync(loginModel.UserName);
+
+            if (userInfo != null && userInfo.Password == loginModel.Password)
+            {
+
+                var token = GenerateJwtToken(userInfo);
+
+                return Ok(token);
+            }
+
+            return Unauthorized();
+        }
+
+        private string GenerateJwtToken(UserInfo userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
