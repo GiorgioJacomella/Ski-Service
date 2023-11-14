a46014ee1b9e78030602770a0c7f1e879d2121f1
Giorgio <91681501+GiorgioJacomella@users.noreply.github.com>
2023-11-14 11:46:45 +0100

    1.02 Implement API End-points

--- SkiService-Backend/SkiService-Backend/Controllers/skiServiceController.cs
+++ SkiService-Backend/SkiService-Backend/Controllers/skiServiceController.cs
@@ -3,11 +3,99 @@ using skiService.Models;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SkiService_Backend.Models;
 
 [Route("api/[controller]")]
 [ApiController]
 public class RegistrationController : ControllerBase
 {
+    private readonly registrationContext _context;
 
+    public RegistrationController(registrationContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Registration
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<registrations>>> GetRegistrations()
+    {
+        return await _context.registrations.ToListAsync();
+    }
+
+    // GET: api/Registration/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<registrations>> GetRegistration(int id)
+    {
+        var registration = await _context.registrations.FindAsync(id);
+
+        if (registration == null)
+        {
+            return NotFound();
+        }
+
+        return registration;
+    }
+
+    // POST: api/Registration
+    [HttpPost]
+    public async Task<ActionResult<registrations>> PostRegistration(registrations registration)
+    {
+        _context.registrations.Add(registration);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetRegistration), new { id = registration.Id }, registration);
+    }
+
+    // PUT: api/Registration/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutRegistration(int id, registrations registration)
+    {
+        if (id != registration.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(registration).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!RegistrationExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/Registration/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteRegistration(int id)
+    {
+        var registration = await _context.registrations.FindAsync(id);
+        if (registration == null)
+        {
+            return NotFound();
+        }
+
+        _context.registrations.Remove(registration);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool RegistrationExists(int id)
+    {
+        return _context.registrations.Any(e => e.Id == id);
+    }
 
 }
--- SkiService-Backend/SkiService-Backend/Program.cs
+++ SkiService-Backend/SkiService-Backend/Program.cs
@@ -1,35 +1,47 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace SkiService_Backend
 {
     public class Program
     {
+        public static string _connectionString;
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+
+            // Build configuration for the connection string
+            var configuration = builder.Configuration;
+            _connectionString = configuration.GetConnectionString("Db1");
+            builder.Services.AddDbContext<registrationContext>(options =>
+                options.UseSqlServer(_connectionString));
+
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+
+
             app.UseHttpsRedirection();
-
+            app.UseCors();
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
