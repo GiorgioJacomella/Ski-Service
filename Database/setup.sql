829139392ca74652a1630dd522f94a6b6144c7f4
Giorgio <91681501+GiorgioJacomella@users.noreply.github.com>
2023-11-14 11:23:39 +0100

    1.02 -Database Setup

---
+++ Database/setup.sql
@@ -0,0 +1,40 @@
+USE master;
+DROP DATABASE IF EXISTS skiService;
+CREATE DATABASE skiService;
+USE skiService;
+
+
+
+--------------------
+CREATE TABLE registrations(
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	[name] VARCHAR(255) not null,
+	[email] VARCHAR(255) not null,
+	[tel] VARCHAR(255) not null,
+	[priority] VARCHAR(255) not null,
+	[service] VARCHAR(255) not null,
+	[startDate] DATE,
+	[finishDate] DATE not null,
+	[status] VARCHAR(255),
+	[note] VARCHAR(255)
+);
+
+
+--login
+----------------------------------------------
+CREATE TABLE userInfo(
+	ID INT IDENTITY(1, 1) PRIMARY KEY,
+	[userName] VARCHAR(255) not null,
+	[password] VARCHAR(255)
+);
+
+CREATE TABLE userSessions (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+	session_key VARCHAR(255),
+	ip_address VARCHAR(45),
+    userID INT,
+    FOREIGN KEY (userID) REFERENCES userInfo(ID)
+);
+
+
+SELECT * FROM registrations;
